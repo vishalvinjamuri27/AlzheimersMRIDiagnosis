e1d5bd65dcfa108638cfd5d3215b5a67a9c7431c
vishalvinjamuri27 <121928736+vishalvinjamuri27@users.noreply.github.com>
2023-09-30 23:58:56 -0400

    Update web.py

--- web.py
+++ web.py
@@ -35,23 +35,6 @@ classifier = KNeighborsClassifier(n_neighbors = 7, metric = 'minkowski', p = 2)
 classifier.fit(x_train, y_train)
 print ('K-Nearest: ', classifier.score(x_test, y_test))
 
-
-# Create a function to predict the class of an MRI scan
-def predict(image_path):
-  # Read and preprocess the image
-  img = image.load_img(image_path, target_size=(224, 224))
-  x = image.img_to_array(img)
-  x = np.expand_dims(x, axis=0)
-  x = x / 255.0
-
-  # Use the model to predict the class
-  predictions = model.predict(x)
-  class_idx = np.argmax(predictions[0])
-
-  # Map the class index back to the original class names
-  classes = ['mildly demented', 'moderately demented', 'non-demented', 'very mildly demented']
-  return classes[class_idx]
-
 # Create the main page
 def main():
   st.title('Creutzfeldt-Jakob Disease Diagnosis')
@@ -62,8 +45,9 @@ def main():
  ptrat= st.text_input("p/t-ratio:")
  abeta= st.text_input("A-beta level:")
  fourteen= st.text_input("14-3-3 level:")
- prediction = predict(uploaded_file)
- st.sidebar.success(f'Prediction: {prediction}')
+new_point=[(ttau,ptau,ptrat,abeta,fourteen)]
+prediction = classifier.predict(new_point)
+st.sidebar.success(f'Prediction: {prediction}')
 
 if __name__ == '__main__':
   main()
